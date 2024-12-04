f1490f1fa0da752a11ef8a33283d6cac2143cad0
Bappy38 <iqbal.bappy.13@gmail.com>
2024-12-04 18:08:41 +0600

    HealthCheck endpoint implemented

--- src/ByteBox.FileStore.API/Program.cs
+++ src/ByteBox.FileStore.API/Program.cs
@@ -1,9 +1,14 @@
 using ByteBox.FileStore.Infrastructure.Extensions;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+builder.Services.AddControllers();
+
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
@@ -16,5 +21,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapControllers();
+
+app.MapHealthChecks("/_health", new HealthCheckOptions
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
