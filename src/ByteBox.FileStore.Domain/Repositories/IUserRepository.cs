fff0f7cade57a05aed5f2936a96e81182999872e
Bappy38 <iqbal.bappy.13@gmail.com>
2024-12-08 22:54:47 +0600

    UserRepository added

---
+++ src/ByteBox.FileStore.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using ByteBox.FileStore.Domain.Entities;
+
+namespace ByteBox.FileStore.Domain.Repositories;
+
+public interface IUserRepository
+{
+    Task AddAsync(User user);
+    Task<User?> GetByEmailAsync(string email);
+}
---
+++ src/ByteBox.FileStore.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using ByteBox.FileStore.Domain.Entities;
+using ByteBox.FileStore.Domain.Repositories;
+using ByteBox.FileStore.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ByteBox.FileStore.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public UserRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _dbContext.Users.AddAsync(user);
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _dbContext.Users
+            .SingleOrDefaultAsync(u => u.Email == email);
+    }
+}
