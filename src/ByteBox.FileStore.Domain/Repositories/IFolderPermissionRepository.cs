31020ec3e211c3d8ec95de2abc422ac00d9740ea
Bappy38 <iqbal.bappy.13@gmail.com>
2025-01-21 15:02:13 +0600

    GetFolderPermissions method added at FolderPermissionRepository

--- src/ByteBox.FileStore.Domain/Repositories/IFolderPermissionRepository.cs
+++ src/ByteBox.FileStore.Domain/Repositories/IFolderPermissionRepository.cs
@@ -1,4 +1,5 @@
-﻿using ByteBox.FileStore.Domain.Entities;
+﻿using ByteBox.FileStore.Domain.DTOs;
+using ByteBox.FileStore.Domain.Entities;
 
 namespace ByteBox.FileStore.Domain.Repositories;
 
@@ -6,5 +7,6 @@ public interface IFolderPermissionRepository
 {
     // INFO:: Follow CRUD order
     Task AddAsync(FolderPermission folderPermission);
+    Task<List<FolderPermissionDto>> GetFolderPermissionsAsync(Guid userId, List<Guid> folderIds);
     Task<bool> HasPermission(Guid userId, Guid folderId);
 }
