e1ef2e92db283a9c8e8f853b866d604bb1e468e8
Bappy38 <iqbal.bappy.13@gmail.com>
2025-02-01 13:41:20 +0600

    VersionId added in File entity so that we can use it in S3 metadata to invalidate presigned url

---
+++ src/ByteBox.FileStore.Infrastructure/Migrations/20250201063315_ThumbnailPresignedUrl, VersionId, IsUploadCompleted props added at File Entity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ByteBox.FileStore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThumbnailPresignedUrlVersionIdIsUploadCompletedpropsaddedatFileEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsUploadCompleted",
+                table: "Files",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ThumbnailPresignedUrl",
+                table: "Files",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "VersionId",
+                table: "Files",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsUploadCompleted",
+                table: "Files");
+
+            migrationBuilder.DropColumn(
+                name: "ThumbnailPresignedUrl",
+                table: "Files");
+
+            migrationBuilder.DropColumn(
+                name: "VersionId",
+                table: "Files");
+        }
+    }
+}
