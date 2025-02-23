13196b24040a00978ebbef967a21a20f9b5606eb
Bappy38 <iqbal.bappy.13@gmail.com>
2025-02-23 19:12:33 +0600

    ThumbnailGenerated message and its handler implemented

---
+++ src/ByteBox.FileStore.Application/MessageHandlers/ThumbnailGeneratedMessageHandler.cs
@@ -0,0 +1,41 @@
+﻿using ByteBox.FileStore.Application.Commands;
+using ByteBox.FileStore.Infrastructure.Messages;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using NexaWrap.SQS.NET.Interfaces;
+
+namespace ByteBox.FileStore.Application.MessageHandlers;
+
+public class ThumbnailGeneratedMessageHandler : IMessageHandler<ThumbnailGeneratedMessage>
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger<ThumbnailGeneratedMessageHandler> _logger;
+
+    public ThumbnailGeneratedMessageHandler(
+        IMediator mediator,
+        ILogger<ThumbnailGeneratedMessageHandler> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+
+    public async Task HandleAsync(ThumbnailGeneratedMessage message)
+    {
+        try
+        {
+            var refreshThumbnailCommand = new RefreshThumbnailPresignedCommand
+            {
+                FileId = message.FileId,
+                ThumbnailKey = message.ThumbnailKey,
+                ThumbnailPresignedUrl = message.ThumbnailPresignedUrl
+            };
+            await _mediator.Send(refreshThumbnailCommand);
+            _logger.LogInformation("ThumbnailGeneratedMessage processed successfully for file with ID: {FileId}", message.FileId);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to process ThumbnailGeneratedMessage for file with ID: {FileId}. ExceptionMessage: {ExceptionMessage}", message.FileId, ex.Message);
+            throw;
+        }
+    }
+}
