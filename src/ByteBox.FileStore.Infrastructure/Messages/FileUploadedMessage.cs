d7cbb84ab4e47b6a8964f15be9d805ac48f9f9f1
Bappy38 <iqbal.bappy.13@gmail.com>
2025-02-24 16:01:17 +0600

    FileUploadedMessage and its handler implemented to sync folder size according to chain

---
+++ src/ByteBox.FileStore.Infrastructure/Messages/FileUploadedMessage.cs
@@ -0,0 +1,15 @@
+﻿using NexaWrap.SQS.NET.Models;
+
+namespace ByteBox.FileStore.Infrastructure.Messages;
+
+public record FileUploadedMessage : IMessage
+{
+    public Guid FileId { get; init; }
+    public double FileSizeInMb { get; init; }
+
+    public Guid FolderId { get; init; }
+
+    public string MessageTypeName => nameof(FileUploadedMessage);
+
+    public string? CorrelationId { get; set; }
+}
