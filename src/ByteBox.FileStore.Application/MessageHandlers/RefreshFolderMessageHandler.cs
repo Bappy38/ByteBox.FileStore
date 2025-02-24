65d8bbad102012cdefe729fb5c09d0c183a75c0b
Bappy38 <iqbal.bappy.13@gmail.com>
2025-02-24 16:00:37 +0600

    RefreshFolderHandler implemented to sync folder size

---
+++ src/ByteBox.FileStore.Application/MessageHandlers/RefreshFolderMessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using ByteBox.FileStore.Application.Commands;
+using ByteBox.FileStore.Infrastructure.Messages;
+using MediatR;
+using NexaWrap.SQS.NET.Interfaces;
+
+namespace ByteBox.FileStore.Application.MessageHandlers;
+
+public class RefreshFolderMessageHandler : IMessageHandler<RefreshFolderMessage>
+{
+    private readonly IMediator _mediator;
+
+    public RefreshFolderMessageHandler(IMediator mediator) => _mediator = mediator;
+
+    public async Task HandleAsync(RefreshFolderMessage message)
+    {
+        var refreshFolderCommand = new RefreshFolderCommand
+        {
+            FolderId = message.FolderId
+        };
+        await _mediator.Send(refreshFolderCommand);
+    }
+}
---
+++ src/ByteBox.FileStore.Infrastructure/Messages/RefreshFolderMessage.cs
@@ -0,0 +1,12 @@
+﻿using NexaWrap.SQS.NET.Models;
+
+namespace ByteBox.FileStore.Infrastructure.Messages;
+
+public record RefreshFolderMessage : IMessage
+{
+    public Guid FolderId { get; set; }
+
+    public string MessageTypeName => nameof(RefreshFolderMessage);
+
+    public string? CorrelationId { get; set; }
+}
